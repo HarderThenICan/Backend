ce5868834567286a3fc09bd1460d46da0d6ed7f0
HarderThenICan <explorer676@gmail.com>
2021-12-13 15:27:03 +0300

    HW4

--- src/test/java/Positive/DeleteTest.java
+++ src/test/java/Positive/DeleteTest.java
@@ -1,43 +1,35 @@
 package Positive;
 
 import MyBase.MyBase;
+import image.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
+import static Endpoins.Endpoints.DELETE;
+import static Endpoins.Endpoints.IMAGE;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
 public class DeleteTest extends MyBase {
-    String uploadedContent;
+    private String uploadedContent;
 
     @BeforeEach
     void deleteUp() {
-        uploadedContent = given()
-                .headers("Authorization", token)
-                .multiPart("image", new File("src/test/resources/images.jpg"))
-                .expect()
-                .body("success", is(true))
-                .statusCode(200)
-                .when()
-                .post("https://api.imgur.com/3/image")
+        uploadedContent = given(headerImage)
+                .post(IMAGE)
                 .then()
                 .extract()
-                .jsonPath()
-                .getString("data.deletehash");
+                .response()
+                .as(Response.class)
+                .getData()
+                .getDeletehash();
+
     }
 
     @DisplayName("Проверка удаления картинки")
     @Test
     void delete() {
-        given()
-                .headers("Authorization", token)
-                .when()
-                .delete("https://api.imgur.com/3/image/{deleteHash}", uploadedContent)
-                .prettyPeek()
-                .then()
-                .statusCode(200);
+        given(header, checkResponse)
+                .delete(DELETE, uploadedContent);
     }
 }
--- src/test/java/Positive/VideoTest.java
+++ src/test/java/Positive/VideoTest.java
@@ -1,33 +1,37 @@
 package Positive;
 
 import MyBase.MyBase;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static Endpoins.Endpoints.UPLOAD;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
 public class VideoTest extends MyBase {
+    private static RequestSpecification headerVideo;
+
+    @BeforeEach
+    void beforeVideo() {
+        headerVideo = new RequestSpecBuilder()
+                .addHeader("Authorization", clienID)
+                .addMultiPart("image", new File("src/test/resources/images.jpeg"))
+                .build();
+
+    }
 
     @DisplayName("Проверка загрузки видео из директории ресурс")
+
+
     @Test
     void getContentInfo() {
-        uploadedContent = given()
-                .headers("Authorization", clienID)
-                .multiPart("video", new File("src/test/resources/hi.mp4"))
-                .expect()
-                .body("success", is(true))
-                .log()
-                .all()
-                .statusCode(200)
-                .when()
-                .post("https://api.imgur.com/3/upload")
-                .then()
-                .extract()
-                .jsonPath()
-                .getString("data.deletehash");
+        given(headerVideo, checkResponse)
+                .post(UPLOAD);
+
 
     }
 }
